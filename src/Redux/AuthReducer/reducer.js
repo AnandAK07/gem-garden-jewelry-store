a7973b50f018759f0eb2107674c9bad0334634b0
AnandAK07 <anandkumark055@gmail.com>
2023-10-17 22:34:40 +0530

    store

--- src/Redux/AuthReducer/reducer.js
+++ src/Redux/AuthReducer/reducer.js
@@ -1,5 +1,13 @@
+import React from 'react'
 const initialState={
     cart:[],
     isAuth:false,
     token:""
+}
+
+
+export const authReducer = (state=initialState,{type,payload}) => {
+  switch (type){
+    default:return state
+  }
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {store} from './Redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
