d44343bcca5f60a9f84052650280cd8de2f736ce
Sushil <sks2669955@gmail.com>
2023-10-18 14:36:12 +0530

    Navbar and Hero section created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './Components/Navbar';
 import AllRoutes from './Routes/AllRoutes';
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import style from "./HomePage.module.css"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <main>
+
+      <section className={`${style.sectionP1} ${style.sectionM1}`} id={`${style.collection}`}>
+        <div className={`${style.heading}`}>
+          <div >
+            <h1>New Collections</h1>
+            <span>Check Out the New Collections of Famous Brands</span>
+          </div>
+
+          <div><p>ALL COLLECTIONS</p></div>
+
+        </div>
+        <div className={`${style.productContainer}`}>
+          <div className={`${style.card}`}>
+            <img className={`${style.cardImg}`} src={require(`../Components/image/card1.png`)} alt="Card 1" />
+            <p className={`${style.cardTitle}`}>Messika</p>
+            <span className={`${style.cardDesc}`} >Move Uno Collection</span>
+          </div>
+
+
+          <div className={`${style.card}`}>
+            <img className={`${style.cardImg}`} src={require(`../Components/image/card2.jpg`)} alt="Card 2" />
+            <p className={`${style.cardTitle}`}>Cartier</p>
+            <span className={`${style.cardDesc}`} >Love Collection</span>
+          </div>
+
+          <div className={`${style.card}`}>
+            <img className={`${style.cardImg}`} src={require(`../Components/image/card3.png`)} alt="Card 3" />
+            <p className={`${style.cardTitle}`}>Garrad</p>
+            <span className={`${style.cardDesc}`} >1735 Collection</span>
+          </div>
+
+          <div className={`${style.card}`}>
+            <img className={`${style.cardImg}`} src={require(`../Components/image/card4.jpg`)} alt="Card 4" />
+            <p className={`${style.cardTitle}`}>Messika</p>
+            <span className={`${style.cardDesc}`} >Lucky Move Collection</span>
+          </div>
+
+
+        </div>
+      </section>
+    </main>
   )
 }
 
