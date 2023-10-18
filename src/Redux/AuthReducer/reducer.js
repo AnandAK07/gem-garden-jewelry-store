4bb8f0674a8089aa9d75da19de1bc58287af89d0
Dilnawaz <mddilnawazalam7739@gmail.com>
2023-10-18 12:31:54 +0530

    Updated AuthReducer

--- src/Redux/AuthReducer/reducer.js
+++ src/Redux/AuthReducer/reducer.js
@@ -1,13 +1,53 @@
-import React from 'react'
-const initialState={
-    cart:[],
-    isAuth:false,
-    token:""
-}
+import React from "react";
+import {
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT,
+} from "./actionTypes";
 
+const initialState = {
+  cart: [],
+  isAuth: false,
+  token: "",
+  loading: false,
+  error: null,
+  success: false,
+};
 
-export const authReducer = (state=initialState,{type,payload}) => {
-  switch (type){
-    default:return state
+export const authReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case LOGIN_REQUEST: {
+      return {
+        ...state,
+        token: "",
+        isAuth: false,
+        loading: false,
+        error: null,
+        success: false,
+      };
+    }
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        token: action.payload,
+        isAuth: true,
+        loading: false,
+        error: null,
+        success: true,
+      };
+    }
+    case LOGIN_FAILURE: {
+      return {
+        ...state,
+        token: "",
+        isAuth: false,
+        loading: false,
+        error: action.payload,
+        success: false,
+      };
+    }
+    default:
+      return state;
   }
-}
+};
