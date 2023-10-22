23831d80e926ab6b0cf3d017c79736de1d704e5e
Dilnawaz <mddilnawazalam7739@gmail.com>
2023-10-23 01:09:31 +0530

    Footer Responsiveness Done Again

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Footer.module.css";
+import styles from "./foot.module.css";
 const Footer = () => {
   return (
     <div className={styles.footerComponent}>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,74 +1,63 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  // const [scrollHeight, setScrollHeight] = useState(0);
 
-    // const [scrollHeight, setScrollHeight] = useState(0);
+  // useEffect(() => {
+  //     const handleScroll = () => {
+  //         setScrollHeight(document.documentElement.scrollTop);
+  //         console.log(scrollHeight)
+  //     };
 
-    // useEffect(() => {
-    //     const handleScroll = () => {
-    //         setScrollHeight(document.documentElement.scrollTop);
-    //         console.log(scrollHeight)
-    //     };
+  //     window.addEventListener("scroll", handleScroll);
 
-    //     window.addEventListener("scroll", handleScroll);
+  //     return () => {
+  //         window.removeEventListener("scroll", handleScroll);
+  //     };
+  // });
 
-    //     return () => {
-    //         window.removeEventListener("scroll", handleScroll);
-    //     };
-    // });
+  // useEffect(() => {
+  //     if (scrollHeight > 15) {
+  //         const nav = document.querySelector("section>div")
+  //         // nav.classList.remove("Navbar_navbar__pCmRo")
+  //         nav.classList.add("Navbar_scrollNav__Pfsh2")
 
-    // useEffect(() => {
-    //     if (scrollHeight > 15) {
-    //         const nav = document.querySelector("section>div")
-    //         // nav.classList.remove("Navbar_navbar__pCmRo")
-    //         nav.classList.add("Navbar_scrollNav__Pfsh2")
-            
-    //     } 
-    //     else if(scrollHeight<=15) {
-    //         const nav = document.querySelector("section>div")
-    //         // nav.classList.add("Navbar_navbar__pCmRo")
-    //         nav.classList.remove("Navbar_scrollNav__Pfsh2")
-    //     }
-    // }, [scrollHeight]);
+  //     }
+  //     else if(scrollHeight<=15) {
+  //         const nav = document.querySelector("section>div")
+  //         // nav.classList.add("Navbar_navbar__pCmRo")
+  //         nav.classList.remove("Navbar_scrollNav__Pfsh2")
+  //     }
+  // }, [scrollHeight]);
 
+  return (
+    <>
+      <section className={`${styles.sectionP1}`}>
+        <div className={`${styles.navbar}`}>
+          <div className={styles.leftNav}>
+            <Link to="/">Jewelry</Link>
+            <Link to="/">Watches</Link>
+            <Link to="/">Gifts</Link>
+          </div>
 
-    return (
-        <>
-            <section className={`${styles.sectionP1}`}>
+          <div className={styles.logoNav}>
+            <Link to="/">
+              <p>GEM GARDEN</p>
+            </Link>
+          </div>
 
-                <div className={`${styles.navbar}` }>
-
-
-                    <div className={styles.leftNav}>
-                        <Link to="/">Jewelry</Link>
-                        <Link to="/">Watches</Link>
-                        <Link to="/">Gifts</Link>
-
-                    </div>
-
-                    <div className={styles.logoNav}>
-                        <Link to="/"><p>GEM GARDEN</p></Link>
-                    </div>
-
-                    <div className={styles.rightNav}>
-                        <Link to="/">Contact Us</Link>
-                        <Link to="/">Account</Link>
-                        <Link to="/cart">Bag</Link>
-
-                    </div>
-
-                </div>
-
-
-
-            </section>
-
-
-        </>
-    )
-}
+          <div className={styles.rightNav}>
+            <Link to="/">Contact Us</Link>
+            <Link to="/">Account</Link>
+            <Link to="/cart">Bag</Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Navbar;
