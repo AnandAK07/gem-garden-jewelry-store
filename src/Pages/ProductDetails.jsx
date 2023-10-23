25fc2a2d4c575cc1fce6286c02e2659255af3928
AnandAK07 <anandkumark055@gmail.com>
2023-10-23 20:48:20 +0530

    css

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -89,8 +89,10 @@ const ProductDetails = () => {
       <div className={styles.details}>
         <h3>{singleItem.title}</h3>
         <p>{singleItem.description}</p>
-        <p>{singleItem.price}</p>
-        <p>{singleItem.origPrice}</p>
+        <div className={styles.price}>
+          <p>{singleItem.price}</p>
+          <p>{singleItem.origPrice}</p>
+        </div>
         {
           itemExists ? <button onClick={()=>{dispatch(removeItemFromCart(id)); setItemExists(false)}}>REMOVE</button> : <button onClick={()=>addToCart(singleItem)}>ADD TO BAG</button>
         }
