29a496b376abe26a6a6bf18d7d370685f814ee2f
Dilnawaz <mddilnawazalam7739@gmail.com>
2023-10-23 20:48:33 +0530

    routing for homepage

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -23,35 +23,33 @@
 
 // export default Products
 
+import React, { useEffect, useState } from "react";
+import { getData } from "../Redux/ProductsReducer/action";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./Products.module.css";
+import { Link, useSearchParams } from "react-router-dom";
 
-
-import React, { useEffect, useState } from 'react'
-import { getData } from '../Redux/ProductsReducer/action'
-import { useDispatch, useSelector } from 'react-redux'
-import styles from './Products.module.css'
-import { Link, useSearchParams } from 'react-router-dom'
-
-import Footer from '../Components/Footer'
+import Footer from "../Components/Footer";
 
 const Products = () => {
   // const [data,setData]=useState([]);
-  const [currentPage,setCurrentPage]=useState(1)
-  const limit=20;
-  let [searchParam,setSearchParam]=useSearchParams()
-  const [category,setCategory]=useState('')
+  const [currentPage, setCurrentPage] = useState(1);
+  const limit = 20;
+  let [searchParam, setSearchParam] = useSearchParams();
+  const [category, setCategory] = useState("");
 
-  const items=useSelector((store)=>store.productReducer.product)
-  console.log(items)
-  const dispatch=useDispatch();
-  useEffect(()=>{
+  const items = useSelector((store) => store.productReducer.product);
+  console.log(items);
+  const dispatch = useDispatch();
+  useEffect(() => {
     // getData(dispatch)
-    dispatch(getData(currentPage,limit))
-  },[currentPage,limit])
-  
-  useEffect(()=>{
-    let params={currentPage,limit};
-    setSearchParam(params)
-  },[currentPage,limit])
+    dispatch(getData(currentPage, limit));
+  }, [currentPage, limit]);
+
+  useEffect(() => {
+    let params = { currentPage, limit };
+    setSearchParam(params);
+  }, [currentPage, limit]);
 
   // const completed=todos.filter((el)=>el.status==true);
 
@@ -59,36 +57,51 @@ const Products = () => {
     <div>
       <div className={styles.pbtns}>
         <div className={styles.filterBtns}>
-          <button disabled={currentPage<=1} onClick={()=>setCurrentPage((prev)=>prev-1)}>prev</button>
+          <button
+            disabled={currentPage <= 1}
+            onClick={() => setCurrentPage((prev) => prev - 1)}
+          >
+            prev
+          </button>
           <button>{currentPage}</button>
-          <button onClick={()=>setCurrentPage((prev)=>prev+1)}>next</button>
+          <button onClick={() => setCurrentPage((prev) => prev + 1)}>
+            next
+          </button>
         </div>
         <div className={styles.filterBtns}>
-          <button disabled={currentPage<=1} onClick={()=>setCurrentPage((prev)=>prev-1)}>prev</button>
+          <button
+            disabled={currentPage <= 1}
+            onClick={() => setCurrentPage((prev) => prev - 1)}
+          >
+            prev
+          </button>
           <button>{currentPage}</button>
-          <button onClick={()=>setCurrentPage((prev)=>prev+1)}>next</button>
+          <button onClick={() => setCurrentPage((prev) => prev + 1)}>
+            next
+          </button>
         </div>
       </div>
-    <div className={styles.pContainer}>
-      
-      {items.map((item,id)=>{
-        return <Link key={id} to={`/products/${item.id}`} >
-                    <div className={styles.child}>
-                    <img src={item.img1} alt="" />
-                    <div className={styles.details}>
-                      <h3 className={styles.space}>{item.title}</h3>
-                      <div className={styles.price}>
-                        <p>₹ {item.price}</p>
-                        <p>₹ {item.origPrice}</p>
-                      </div>
-                      </div>
-                    </div>
-                </Link>
-      })}
+      <div className={styles.pContainer}>
+        {items.map((item, id) => {
+          return (
+            <Link key={id} to={`/products/${item.id}`}>
+              <div className={styles.child}>
+                <img src={item.img1} alt="" />
+                <div className={styles.details}>
+                  <h3 className={styles.space}>{item.title}</h3>
+                  <div className={styles.price}>
+                    <p>₹ {item.price}</p>
+                    <p>₹ {item.origPrice}</p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+      <Footer />
     </div>
-    <Footer/>
-    </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
