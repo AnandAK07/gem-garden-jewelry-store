152fb6168d691c06fc518f34ee4a8ca7d7a1aa96
Sushil <sks2669955@gmail.com>
2023-10-23 16:09:54 +0530

    responsiveness added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,45 +5,18 @@ import styles from "./Navbar.module.css"
 
 const Navbar = () => {
 
-    // const [scrollHeight, setScrollHeight] = useState(0);
 
-    // useEffect(() => {
-    //     const handleScroll = () => {
-    //         setScrollHeight(document.documentElement.scrollTop);
-    //         console.log(scrollHeight)
-    //     };
-
-    //     window.addEventListener("scroll", handleScroll);
-
-    //     return () => {
-    //         window.removeEventListener("scroll", handleScroll);
-    //     };
-    // });
-
-    // useEffect(() => {
-    //     if (scrollHeight > 15) {
-    //         const nav = document.querySelector("section>div")
-    //         // nav.classList.remove("Navbar_navbar__pCmRo")
-    //         nav.classList.add("Navbar_scrollNav__Pfsh2")
-            
-    //     } 
-    //     else if(scrollHeight<=15) {
-    //         const nav = document.querySelector("section>div")
-    //         // nav.classList.add("Navbar_navbar__pCmRo")
-    //         nav.classList.remove("Navbar_scrollNav__Pfsh2")
-    //     }
-    // }, [scrollHeight]);
 
 
     return (
-        <>
-            <section className={`${styles.sectionP1}`}>
+        
+           
 
-                <div className={`${styles.navbar}` }>
+                <div className={`${styles.navbar}` } id='navbar'>
 
 
                     <div className={styles.leftNav}>
-                        <Link to="/">Jewelry</Link>
+                        <Link to="/products">Jewelry</Link>
                         <Link to="/">Watches</Link>
                         <Link to="/">Gifts</Link>
 
@@ -64,10 +37,10 @@ const Navbar = () => {
 
 
 
-            </section>
+            
 
 
-        </>
+        
     )
 }
 
